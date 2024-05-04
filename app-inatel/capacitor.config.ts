08ed8d60fc02cc2bcb1b273deb9b471fdf9f18cd
lrgsouza <lucas_souza@outlook.com>
2024-05-03 23:07:47 -0300

    Add initial app

---
+++ app-inatel/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.inatel',
+  appName: 'app-inatel',
+  webDir: 'www'
+};
+
+export default config;
