3b0d5fcd02dbdb1f8a736836fe05ad795f413ad1
lrgsouza <lucas_souza@outlook.com>
2024-05-04 18:25:08 -0300

    Change ionic from standalone to ngmodules

--- app-inatel/src/app/tabs/tabs.page.ts
+++ app-inatel/src/app/tabs/tabs.page.ts
@@ -1,19 +1,12 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { home, calendarClear, settings, heartCircle, personCircle } from 'ionicons/icons';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss'],
-  standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {
-    addIcons({ home, calendarClear, settings, heartCircle, personCircle });
-  }
+  constructor() {}
+
 }
